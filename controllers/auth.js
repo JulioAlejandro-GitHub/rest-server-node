6ef40a416977618d92f1ed0d3afbd83027dec1b0
Julio Alejandro Morales Gutierrez <juliomoralesgutierrez@gmail.com>
2021-05-16 08:31:30 -0400

    google singin

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const { response } = require("express");
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
     const {correo, password} = req.body;
@@ -43,6 +44,53 @@ const login = async(req, res = response) => {
     }
 
 }
+const googleSingIn = async(req, res = response) => {
+
+    const {id_token} = req.body;
+
+    try {
+
+        const googleUser = await googleVerify(id_token);
+
+        /*
+        autenticar o crear?
+        */
+       let usuario = await Usuario.findOne({correo:googleUser.correo});
+       if (!usuario) {//crear
+            const data = {
+                correo:googleUser.correo,
+                nombre:googleUser.nombre,
+                img:googleUser.img,
+                password:'googleSingIn',
+                google:true
+            };
+            // instancia de coleccion tabla
+            usuario = new Usuario(data);
+            //graba el objeto en coleccion
+            await usuario.save();
+       }
+
+       if (!usuario.estado) {//verifica estado en DB
+            res.status(401).json({
+                'msg':'usuario bloqueado'
+            }); 
+       }
+        // generar sesion JWT
+        const token = await generarJWT(usuario._id);
+        res.status(200).json({
+            'msg':'ok google SingIn',
+            token
+        }); 
+    } catch(error) {
+        res.status(400).json({
+            'msg':'error token google SingIn'
+        }); 
+    }
+    
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
